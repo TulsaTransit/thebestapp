7e28e08fe4bd407a3fbc7a1a1b757786ad0e25f7
Harold Kurth <harold.kurth@gmail.com>
2015-07-20 10:08:14 -0500

    operand
    added operand keys

--- thebestapp/ViewController.swift
+++ thebestapp/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    var userIsInMiddleofTypingANumber : Bool = false
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInMiddleofTypingANumber{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsInMiddleofTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation{
+            case "✖️":
+                if operandStack.count >= 2{
+                    displayValue = operandStack.removeLast() * operandStack.removeLast()
+                    enter()
+                }
+//            case "➗":
+//            case "➕":
+//            case "➖":
+            default: break
+        }
     }
-
-
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInMiddleofTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsInMiddleofTypingANumber   = false
+        }
+    }
+    
 }
 
